a86861f33fc55d066dc4539de7201611c4978194
Uanderson <uandersonsouza140@gmail.com>
2026-02-10 16:53:01 -0300

    chore: remove .env from repo and add to gitignore

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const fastify = require('fastify')({ logger: true });
+
+// Basic route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+// Start server
+const start = async () => {
+  try {
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
